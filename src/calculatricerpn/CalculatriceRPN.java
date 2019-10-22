f817df6f2adc4d044d13cd9044a203e90017149f
omydba226 <yassinemamadou97@gmail.com>
2019-10-22 22:48:09 +0200

    deuxieme validation

---
+++ src/calculatricerpn/CalculatriceRPN.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatricerpn;
+import exeption.SAISIException;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ *
+ * @author HP
+ */
+public class CalculatriceRPN {
+    
+    	private MoteurRPN cal;
+    	Stack<Double> p = new Stack<> ();//creation de la pile devant servir à stocker les operandes
+    	
+    	public CalculatriceRPN(){
+			cal = new MoteurRPN();
+		}
+    	
+    	public MoteurRPN getMoteur() {
+    		return cal;
+    	}
+    	
+        
+        
+        //cette fonction renvoie vrai si l'utilisateur saisit une bonne operande 
+    	public boolean IsOperande(String sign)
+    	{
+    		try{
+    			Double value= Double.parseDouble(sign);
+    		}catch (Exception e) {
+				return false;
+			}
+			return true;
+    	}
+    	
+    	public boolean IsClean(String sign){
+    		return sign.equals("c") || sign.equals("clean") ||sign.equals("C");
+    	}
+    	
+    	public boolean IsUndo(String sign){
+    		 return sign.equals("S")|| sign.equals("s") || sign.equals("supprimer");
+    	}
+		
+		public void saisie() throws SAISIException{ 
+			Scanner sc=new Scanner(System.in);
+			int Compteur=0;
+			do{				
+				System.out.println("Veuillez saisir un nombre  ou un operateur:");
+				String sign = sc.nextLine();
+				if(sign.equals("exit")) return ;
+				
+				 if(IsOperande(sign)){
+					cal.enregistrer(Double.parseDouble(sign));
+				}
+				
+				else if (IsClean(sign)) {
+					cal=new MoteurRPN();
+					Compteur=0;
+				}
+
+				else if (IsUndo(sign)) cal.depile();
+				
+				else if (operateur(sign)){						
+					if ( cal.nbrOperande()-1>Compteur ) {
+						Compteur++;
+						cal.calcul(signe(sign));
+						Compteur=0;						
+					}					
+				} else
+					try {
+						throw new SAISIException("Erreur de saisie.Veuillez saisir un nombre réel \n");
+					} catch (SAISIException e) {
+						e.printStackTrace();
+					}
+				System.out.println("\nLa pile contient:"+cal.getP());
+				}while(true);
+			}
+
+	public Operation signe(String sign) {
+		switch (sign) {
+        case "+":
+            return Operation.ADD;
+        case "-":
+            return Operation.SUB;
+        case "*":
+            return Operation.MULT;
+        case "/":
+            return Operation.DIV;
+            }
+		return null;
+	}
+		
+	  private boolean operateur(String token)
+		     { 
+		      return (token.equals("+") || token.equals("-") || 
+		        token.equals("*") || token.equals("/"));
+			}	
+			private MoteurRPN cal;
+    	Stack<Double> p = new Stack<> ();//creation de la pile devant servir à stocker les operandes
+    	
+    	public SaisieRPN(){
+			cal = new MoteurRPN();
+		}
+    	
+    	public MoteurRPN getMoteur() {
+    		return cal;
+    	}
+    	
+        
+        
+        //cette fonction renvoie vrai si l'utilisateur saisit une bonne operande 
+    	public boolean IsOperande(String sign)
+    	{
+    		try{
+    			Double value= Double.parseDouble(sign);
+    		}catch (Exception e) {
+				return false;
+			}
+			return true;
+    	}
+    	
+    	public boolean IsClean(String sign){
+    		return sign.equals("c") || sign.equals("clean") ||sign.equals("C");
+    	}
+    	
+    	public boolean IsUndo(String sign){
+    		 return sign.equals("S")|| sign.equals("s") || sign.equals("supprimer");
+    	}
+		
+		public void saisie() throws SAISIEException{ 
+			Scanner sc=new Scanner(System.in);
+			int Compteur=0;
+			do{				
+				System.out.println("Veuillez saisir un nombre  ou un operateur:");
+				String sign = sc.nextLine();
+				if(sign.equals("exit")) return ;
+				
+				 if(IsOperande(sign)){
+					cal.enregistrer(Double.parseDouble(sign));
+				}
+				
+				else if (IsClean(sign)) {
+					cal=new MoteurRPN();
+					Compteur=0;
+				}
+
+				else if (IsUndo(sign)) cal.depile();
+				
+				else if (operateur(sign)){						
+					if ( cal.nbrOperande()-1>Compteur ) {
+						Compteur++;
+						cal.calcul(signe(sign));
+						Compteur=0;						
+					}					
+				} else
+					try {
+						throw new SAISIEException("Erreur de saisie.Veuillez saisir un nombre réel \n");
+					} catch (SAISIEException e) {
+						e.printStackTrace();
+					}
+				System.out.println("\nLa pile contient:"+cal.getP());
+				}while(true);
+			}
+
+	public Operation signe(String sign) {
+		switch (sign) {
+        case "+":
+            return Operation.ADD;
+        case "-":
+            return Operation.SUB;
+        case "*":
+            return Operation.MULT;
+        case "/":
+            return Operation.DIV;
+            }
+		return null;
+	}
+		
+	  private boolean operateur(String token)
+		     { 
+		      return (token.equals("+") || token.equals("-") || 
+		        token.equals("*") || token.equals("/"));
+			}	
+		
+    
+}
